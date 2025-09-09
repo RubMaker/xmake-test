b8094b7f5bdaab5a7d96f352efb5df6a9c39e528
wgx.rubmaker@gmail.com <wgx@wgxs-MacBook-Air.local>
2025-09-09 21:57:23 +0800

    add qt6

---
+++ qt6test/xmake.lua
@@ -0,0 +1,42 @@
+set_version("1.0.0")
+add_rules("mode.debug", "mode.release")
+
+includes("@builtin/xpack")
+
+add_requires("qt6widgets")
+
+target("test")
+    add_rules("qt.widgetapp")
+    add_packages("qt6widgets")
+
+    add_includedirs("include")
+    add_headerfiles("include/*.h")
+    add_files("src/*.cpp")
+    add_files("src/mainwindow.ui")
+    -- add files with Q_OBJECT meta (only for qt.moc)
+    add_files("include/mainwindow.h")
+   
+   
+  xpack("test")
+    set_formats("zip", "targz", "srczip", "srctargz")
+    --set_formats("zip", "targz", "runself")
+    set_title("test")
+    set_author("wgx")
+    set_description("A test installer.")
+    set_homepage("https://xmake.io")
+    set_licensefile("LICENSE.md")
+    add_targets("test")
+    add_installfiles("src/(assets/*.png)", {prefixdir = "images"})
+    add_sourcefiles("(src/**)")
+    set_iconname("xmake")
+
+    after_installcmd(function (package, batchcmds)
+        batchcmds:mkdir(package:installdir("resources"))
+        batchcmds:cp("src/assets/*.txt", package:installdir("resources"), {rootdir = "src"})
+        batchcmds:mkdir(package:installdir("stub"))
+    end)
+
+    after_uninstallcmd(function (package, batchcmds)
+        batchcmds:rmdir(package:installdir("resources"))
+        batchcmds:rmdir(package:installdir("stub"))
+    end)
